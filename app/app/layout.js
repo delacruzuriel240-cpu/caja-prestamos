8c6bf3fb389f13ae897199a16b33a18f96e338db
delacruzuriel240-cpu <delacruzuriel240@gmail.com>
2026-05-11 17:09:56 -0600

    Create layout.js

---
+++ app/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Caja Prestamos',
+  description: 'App de prestamos',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  )
+}
